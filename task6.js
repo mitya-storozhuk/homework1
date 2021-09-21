2252583f2e0d77c46518f8be71a07ffe85b7265b
mitya-storozhuk <88045766+mitya-storozhuk@users.noreply.github.com>
2021-09-21 22:14:24 +0300

    Create task6.js

---
+++ task6.js
@@ -0,0 +1,14 @@
+//task6
+let myName;
+let myLastName;
+let taskGroupe;
+let birthYear;
+myName = "Dmytro";
+myLastName = "Storozhuk";
+taskGroupe = "Lv-636.JS Core";
+birthYear = 1990;
+let isMarried = true;
+console.log(typeof birthYear, typeof isMarried, typeof myName, typeof myLastName, typeof taskGroupe); 
+let dataNull = null;
+let unknownData;
+console.log(typeof dataNull, typeof unknownData);
