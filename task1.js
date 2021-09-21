76c8e5b6fd304fe46b97ec99a87388ab1660d3db
mitya-storozhuk <88045766+mitya-storozhuk@users.noreply.github.com>
2021-09-21 21:57:28 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,1 +1,26 @@
+//task2
 console.log('Storozhuk');
+
+//task3
+let data1;
+let data2;
+data1 = 100;
+data2 = "one hundred";
+alert(data1);
+alert(data2);
+data1 = data2;
+alert(data1);
+alert(data2);
+
+//task4
+const typeObj = {
+    String: "string",
+    Number: 10,
+    Boolean: true,
+    Undefined: undefined,
+    Null: null
+};
+
+//task5
+const isAdult = prompt ("What is your age");
+console.log(isAdult);
